6a1d3730ff1a782b8bcd47896e7083237c7594bc
Fabibourdokan <fabianabourdokanvaliente@gmail.com>
2022-07-07 19:28:19 -0300

    desafio entregue

---
+++ script.js
@@ -0,0 +1,11 @@
+for(let i= 1; i<=10; i++)
+{console.log(i)}
+
+let number= 10
+while(number>=1)
+{console.log(number); number--}
+for(let numero=1; numero<=100; numero++)
+{if(numero%2 === 0) console.log(numero)}
+
+for(let numero=1; numero<=100; numero++)
+{if(numero%3 === 0) console.log(numero)}
